a74a92cce0653aac773f3a1a11c92d698a28936b
Aziz Özenir <75263139+AzizOzenir@users.noreply.github.com>
2023-06-17 14:07:11 +0300

    Update home.js

--- cat/cat/src/pages/home/home.js
+++ cat/cat/src/pages/home/home.js
@@ -2,32 +2,23 @@ import React, { useState, useEffect } from 'react'
 import { Link } from "react-router-dom"
 import "./home.css"
 
-
 function Home() {
   const [cats, setCats] = useState([]);
   const [searchValue, setSearchValue] = useState("");
   const [isSearched, setisSearched] = useState(false);
 
   useEffect(() => {
-
-   
-
-
     const fetchData = async () => {
       try {
         const res = await fetch("https://api.thecatapi.com/v1/breeds");
         const data = await res.json()
         setCats(data)
-
-
       } catch (e) {
       }
       setisSearched(false);
     }
 
     fetchData()
-
-
   }, [])
 
   const changeController =  (e)=>{
@@ -35,8 +26,6 @@ function Home() {
   }
   const handleSubmit = async (event) => {
     event.preventDefault();
-    
-   
       try {
         const res = await fetch(`https://api.thecatapi.com/v1/breeds/search?q=${searchValue}`);
         const data = await res.json()
@@ -44,12 +33,9 @@ function Home() {
         setisSearched(true);
         console.log(searchValue)
       } catch (e) {
-  
       }
-    
   }
   return (
-
     <>
       {!cats ? (<h1 id='title' className='flex items-center justify-center text-slate-900 text-center text-3xl px-5 py-5 h-screen font-semibold'>Loading..</h1>)
         : (
@@ -58,15 +44,12 @@ function Home() {
               <h1 id='title' className='flex items-center justify-center  text-center  text-3xl px-5 py-1 font-semibold' > Cat Info App </h1>
               <from onSubmit={ handleSubmit}  className="flex items-center justify-center py-2" >
                 <input   value={searchValue}    onChange={changeController}   type="text" className=' bg-gray-300 py-2 px-16 rounded' name='Search ' placeholder='Search a Cat Breed...' />
-                <button type='submit' onClick={handleSubmit} className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-r"
-      >
+                <button type='submit' onClick={handleSubmit} className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-r">
         Submit
       </button>
               </from>
             </>
             <>
-          
-
               <div className='grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3'>
                 {!isSearched ? cats.map((cat) => {
 
