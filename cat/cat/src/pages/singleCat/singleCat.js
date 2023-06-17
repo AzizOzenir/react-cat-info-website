22b0a66e7b90b1c36dd1ef46450eeb9df16b671f
Aziz Özenir <75263139+AzizOzenir@users.noreply.github.com>
2023-06-17 14:05:54 +0300

    Update singleCat.js
    a quick check on codes

--- cat/cat/src/pages/singleCat/singleCat.js
+++ cat/cat/src/pages/singleCat/singleCat.js
@@ -6,38 +6,25 @@ function SingleCat() {
     const [cat, setCat] = useState({});
     const { name } = useParams();
 
-
     useEffect(() => {
         const fetchDataOfCat = async () => {
             try {
                 const res = await fetch(`https://api.thecatapi.com/v1/breeds/search?q=${name}`);
                 const data = await res.json();
 
-console.log(data[0])
-
                 setCat(data[0]);
             } catch (error) {
                 console.log("error", error);
             }
-
         }
-
         fetchDataOfCat();
-
-        console.log(cat);
-
-
-
     }, [])
 
     return (
         <>
             <div className='grid gap-6 p-6 grid-cols-1 xl:grid-cols-2 m-4  h-screen'>
-
                 <img className="m-2 max-w-2xl w-full max-w-screen-1/2 rounded" src={`https://cdn2.thecatapi.com/images/${cat.reference_image_id}.jpg`} alt={cat.id} />
-
                 <ul class="max-w-md space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400">
-                
                     <LevelIndicator characteristic={"Affection Level"} level={cat.affection_level}/>
                     <LevelIndicator characteristic={"Adaptability Level"} level={cat.adaptability}/>
                     <LevelIndicator characteristic={"Child Friendly"} level={cat.child_friendly}/>
@@ -52,12 +39,6 @@ console.log(data[0])
                 <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Origin: {cat.origin}</span>
                 <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Life Expectancy: {cat.life_span + " years"}</span>
                  <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"> {cat.description}</span>
-
-
-
-
-
-
                 <Link to="/">
                     <button type="button" class="py-2.5 w-40 h-12  px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" >Go Back</button>
                 </Link>
